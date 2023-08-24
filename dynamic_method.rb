0a4c95213e649ec590791adaaabcbed5f742f568
Somesh Yadav <soyadav@bestpeers.com>
2023-08-24 23:50:53 +0530

    problem: make a dynamic method to do arithmetic operations

---
+++ dynamic_method.rb
@@ -0,0 +1,3 @@
+=begin
+Problem - make a dynamic method to do arithmetic operations, where a and b as a variable will be passed and +,-,'*' all these operations will also be passed as operation command?
+=end
